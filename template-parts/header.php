2adcb855915b101317edc62466eca055c5190930
Fernando Tessmann <nandotess85@gmail.com>
2017-11-05 10:19:40 -0600

    Base

--- template-parts/header.php
+++ template-parts/header.php
@@ -11,6 +11,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<meta name="viewport" content="width=device-width, user-scalable=no, maximum-scale=1, initial-scale=1">
 		<?php wp_head(); ?>
 	</head>
 
